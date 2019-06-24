ea81d7cbd191b1e981832efc7e5252802a0398b0
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-06-23 23:53:12 -0700

    add event handler for switching animation and still image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,17 +2,26 @@ let keyWords = ["kitty", "puppy", "bird", "fish", "piggy"];
 let gifLimit = 10;
 let rating = "g";
 
-// start of jquery
-$(document).ready(function () {
+function createButtons() {
     // create buttons
+    $("#buttonsshow").empty();
     for (i = 0; i < keyWords.length; i++) {
-        $("#buttonsshow").append('<button id="' + keyWords[i] + '">' + keyWords[i] + '</button>');
+        let newButton = $("<button>");
+        newButton.attr("id", keyWords[i]);
+        newButton.text(keyWords[i]);
+        $("#buttonsshow").append(newButton);
     }
     $("#buttonsshow>*").each(function () {
         $(this).css('margin-right', '10px');
     })
+}
+
+// start of jquery
+$(document).ready(function () {
+    createButtons();
     $("#buttonsshow>*").each(function () {
-        $(this).on('click', function () {
+        $(this).on('click', function (event) {
+            console.log("click " + this.id);
             let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q=" + $(this).text() + "&limit=" + gifLimit + "&rating=" + rating;
             let buttonText = $(this).text();
             $.ajax({
@@ -21,13 +30,46 @@ $(document).ready(function () {
             }).then(function (response) {
                 $("#main").empty();
                 for (i = 0; i < response.data.length; i++) {
-                    let imageURL = response.data[i].images.fixed_height.url;
+                    console.log(response.data[i]);
+                    let imageURLAnimate = response.data[i].images.fixed_height.url;
+                    let imageURLStill = response.data[i].images.fixed_height_still.url;
                     let gifImage = $("<img>");
-                    gifImage.attr("src", imageURL);
+                    gifImage.attr("src", imageURLStill);
+                    gifImage.attr("data-still", imageURLStill);
+                    gifImage.attr("data-animate", imageURLAnimate);
+                    gifImage.attr("data-state", "still");
                     gifImage.attr("alt", buttonText);
+                    gifImage.attr("class", "gif");
+                    gifImage.on("click", function () {
+                        console.log("gif clicked");
+                        //console.log($(this));
+                        let state = $(this).attr("data-state");
+                        console.log(state);
+                        if (state === "still") {
+                            state = "animate";
+                            let animateURL = $(this).attr("data-animate");
+                            $(this).attr("src", animateURL);
+                        } else {
+                            state = "still";
+                            let stillURL = $(this).attr("data-still");
+                            $(this).attr("src", stillURL);
+                        }
+                        $(this).attr("data-state", state);
+                    })
                     $("#main").append(gifImage);
                 }
             });
+            return true;
         });
+    })
+
+    $("button[id='submit']").on("click", function (event) {
+        event.stopPropagation();
+        let newKeyword = $("input[id='newKeyword']").val().trim();
+        if (false === keyWords.includes(newKeyword, 0)) {
+            keyWords.push(newKeyword);
+        }
+        createButtons();
+        //event.preventDefault();
     })
 }); 
