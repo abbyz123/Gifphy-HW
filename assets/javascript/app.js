55ef30b600366bdc7888d0befa8b139581c7cec2
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-06-23 20:53:09 -0700

    add click event handler of fetching 10 gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,33 @@
 let keyWords = ["kitty", "puppy", "bird", "fish", "piggy"];
+let gifLimit = 10;
+let rating = "g";
 
 // start of jquery
-$(document).ready(function() {
+$(document).ready(function () {
     // create buttons
     for (i = 0; i < keyWords.length; i++) {
         $("#buttonsshow").append('<button id="' + keyWords[i] + '">' + keyWords[i] + '</button>');
     }
-    $("#buttonsshow>*").each(function(){
-        $(this).css('margin-right','10px');
+    $("#buttonsshow>*").each(function () {
+        $(this).css('margin-right', '10px');
+    })
+    $("#buttonsshow>*").each(function () {
+        $(this).on('click', function () {
+            let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q=" + $(this).text() + "&limit=" + gifLimit + "&rating=" + rating;
+            let buttonText = $(this).text();
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                $("#main").empty();
+                for (i = 0; i < response.data.length; i++) {
+                    let imageURL = response.data[i].images.fixed_height.url;
+                    let gifImage = $("<img>");
+                    gifImage.attr("src", imageURL);
+                    gifImage.attr("alt", buttonText);
+                    $("#main").append(gifImage);
+                }
+            });
+        });
     })
 }); 
